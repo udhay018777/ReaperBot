cdce038cb76386ac921d08b8df13a08d94beaf24
udhay018777 <63841073+udhay018777@users.noreply.github.com>
2020-06-07 13:19:18 +0530

    Create dubeyji.py

---
+++ userbot/plugins/dubeyji.py
@@ -0,0 +1,49 @@
+from telethon import events
+
+
+
+import asyncio
+
+
+
+from userbot.utils import admin_cmd
+
+
+
+@borg.on(admin_cmd("dubey"))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1.0
+
+    animation_ttl = range(0,36)
+
+    #input_str = event.pattern_match.group(1)
+
+   # if input_str == "dubey":
+
+    await event.edit("Dubey Joins the chat")
+
+    animation_chars = [
+
+            "Allahabad wale",
+
+            "**Ek Jhapad Mar du toh Saale Mu ke Dant Gaand se nikalenge**"
+
+         ]
+
+            
+
+
+
+    for i in animation_ttl:
+
+        	
+
+        await asyncio.sleep(animation_interval)
+
+        await event.edit(animation_chars[i % 18])
